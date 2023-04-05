9691694c432c8f5d6e99674c1f2add828603354d
Marco Antonio Guzman <marco_antonio_guzman@epam.com>
2023-04-05 11:46:48 -0600

    Adding index to show lesson apps. Updating unit tests, snapshot and integration tests because of reorganization.

--- lesson1/coreconcepts/src/index.js
+++ lesson1/coreconcepts/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import FirstLessonApp from './components/firstLessonApp';
 import reportWebVitals from './reportWebVitals';
+import ThirdLessonApp from './components/thirdLessonApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<ul>
+            <br />
+            <li>
+              <Link to='lesson1' target='_blank'>
+                Core Concepts
+              </Link>
+            </li>
+            <br />
+            <li>
+              <Link to='lesson3' target='_blank'>
+                Components
+              </Link>
+            </li>
+          </ul>}>
+        </Route>
+        <Route exact path='/lesson3' element={<ThirdLessonApp />}></Route>
+        <Route exact path='/lesson1' element={<FirstLessonApp />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
